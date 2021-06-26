88a90d5c41a8fec6dbe1eb81c957fa5220443a0e
khauminhduy <khauminhduy@gmail.com>
2021-06-26 21:42:57 +0700

    update

--- src/main/java/com/khauminhduy/streamapi/ReduceDemo.java
+++ src/main/java/com/khauminhduy/streamapi/ReduceDemo.java
@@ -1,10 +1,26 @@
 package com.khauminhduy.streamapi;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class ReduceDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		Integer sum = numbers.stream()
+			   .reduce(0, Integer::sum);
+		System.out.println(sum);
+		
+		Optional<Integer> max = numbers.stream()
+			   .reduce(Integer::max);
+		
+		max.ifPresentOrElse(System.out::println, () -> System.out.println("Not Value"));
+		
+		Optional<Integer> min = numbers.stream()
+			   .reduce(Integer::min);
+		
+		min.ifPresentOrElse(System.out::println, () -> System.out.println("Not Value"));
 	}
 
 }
