8051402a19320e5059f9e9c84f9c47e719e4686b
khauminhduy <khauminhduy@gmail.com>
2021-06-27 15:09:36 +0700

    update

--- src/main/java/com/khauminhduy/stringiobitpieces/StringJoinerDemo.java
+++ src/main/java/com/khauminhduy/stringiobitpieces/StringJoinerDemo.java
@@ -5,10 +5,20 @@ import java.util.StringJoiner;
 public class StringJoinerDemo {
 
 	public static void main(String[] args) {
-		StringJoiner stringJoiner = new StringJoiner(", ");
+		StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
 		stringJoiner.add("Khau Minh Duy").add("Nguyen Van A").add("Nguyen Van B");
 		String names = stringJoiner.toString();
 		System.out.println(names);
+		
+		StringJoiner stringJoiner2 = new StringJoiner(", ");
+		stringJoiner2.add("Khau Minh Duy").add("Nguyen Van A").add("Nguyen Van B");
+		String names2 = stringJoiner2.toString();
+		System.out.println(names2);
+		
+		String[] strings = {"One", "Two", "Three", "Four"};
+		String join = String.join(", ", strings);
+		System.out.println(join);
+		
 	}
 	
 }
